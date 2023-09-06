e6a28ee554285284bea5c46d017ad81931a48b98
Daniel Ferrari <ferrari.dg@gmail.com>
2023-09-06 14:48:07 +0100

    feat: Create func to test load GH data

---
+++ src/load_data.py
@@ -0,0 +1,25 @@
+"""Load Github repositories data."""
+from os import getenv
+import json
+
+from src.tools.commons import (
+    load_settings,
+    load_repositories,
+)
+
+DASHBOARD_SETTINGS_FILE = getenv("DASHBOARD_SETTINGS_FILE", "settings.json")
+DASHBOARD_MAX_WORKERS = int(getenv("DASHBOARD_MAX_WORKERS", 4))
+
+SETTINGS = load_settings(DASHBOARD_SETTINGS_FILE)
+DASHBOARD_REPOS = load_repositories(SETTINGS.organization, SETTINGS.tabs, DASHBOARD_MAX_WORKERS)
+
+with open("final_settings.json", "w") as file:
+    json.dump(SETTINGS.model_dump(), file, indent=4, default=str)
+
+with open("final_repos.json", "w") as file:
+    file.write("[\n")
+    file.write(",\n".join([
+        json.dumps(repo.model_dump(), indent=4, default=str)
+        for repo in DASHBOARD_REPOS.values()
+    ]))
+    file.write("\n]")
